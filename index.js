9e62c380f2f079e06aae6a0e61cc491fd5eee993
egsir1 <egsir111@gmail.com>
2023-05-04 19:49:49 +0900

    fix External packages file added

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ fs.readFile("sample.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log("Fayl malumoti =>", data);
 });
+
+// externel
+const moment = require("moment");
+
+const time = moment().format("HH:mm:ss");
+console.log(time);
